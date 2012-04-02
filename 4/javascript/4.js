4270ab82353c6b8c089e3ceee9a91a09fb7cb2ce
Nathan Beddes <beddes@gmail.com>
2012-04-02 12:51:14 -0400

    Finished adding largest palindromic number finder.

--- 4/javascript/4.js
+++ 4/javascript/4.js
@@ -8,3 +8,41 @@
   Answer:
   906609
 */
+
+var checkPalindrome = function (word)
+{
+   var end = word.length - 1;
+   var start = 0;
+
+   while (start < end) {
+      if (word.charAt(start) != word.charAt(end)) {
+         return false;
+      }
+      ++start;
+      --end;
+   }
+   
+   return true;
+};
+
+
+var highestCandidate = 0;
+var highestX = 0;
+var highestY = 0;
+var xStop = 0;
+for (var x = 999; x > xStop; --x) {
+   for(var y = x; y > 0; --y) {
+      var candidate = x*y;
+      if (checkPalindrome(candidate.toString())) {
+         if (candidate > highestCandidate) {
+            highestCandidate = candidate;
+            xStop = y;
+            highestX = x;
+            highestY = y;
+         }
+         break;
+      }
+   }
+}
+
+console.log(highestX+"*"+highestY+": "+highestCandidate);
