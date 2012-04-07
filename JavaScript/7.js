76fce6657fad32b815287575ecba187265ed01e8
Nathan Beddes <beddes@gmail.com>
2012-04-06 22:41:09 -0400

    Changed atkin.js to prime.js, to group all prime number stuff in one place. Added to prime.js for problem 7.

--- JavaScript/7.js
+++ JavaScript/7.js
@@ -1,19 +1,17 @@
 /*
-By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
-the 6th prime is 13.
+  By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
+  that the 6th prime is 13.
 
-What is the 10001st prime number?
+  What is the 10001st prime number?
 */
 /*
-Answer:
-104743
+  Answer:
+  104743
 */
 
 
-// Use the atkin module. For nodejs only.
-var sieve = require('./atkin.js');
-
-
-var firstPrimes = sieve.sieveOfAtkin(1000);
-console.log(firstPrimes);
-console.log(firstPrimes.length);
+/*
+  Use the primes module. This only works with nodejs.
+*/
+var primes = require('./primes.js');
+console.log(primes.findNthPrime(10001));
---
+++ JavaScript/primes.js
@@ -0,0 +1,126 @@
+/* 
+   Node module for various prime finding utilities.
+
+   The 'exports' prefix on lines declaring functions make that function
+   available in the primes nodejs module. 
+
+   Note that there is 'var' declarations outside of functions would make those
+   variables private to this module.
+*/
+
+
+/*
+ sieveOfAtkin() finds all primes up to, and including, a number.
+
+ http://en.wikipedia.org/wiki/Sieve_of_Atkin
+*/
+exports.sieveOfAtkin = function (limit)
+{
+    /*
+      Initialize the sieve.
+    */
+    var isPrime = [false, false, true, true, false];
+    for (var i = 5; i <= limit; ++i) {
+	isPrime.push(false);
+    }
+    var sqrtLimit = Math.floor(Math.sqrt(limit));
+
+    /*
+      Put in candidate primes:
+      Integers which have an odd number of representations by certain quadratic
+      forms.
+     */
+    var n;
+    var x;
+    var y;
+    for (x = 1; x <= sqrtLimit; ++x) {
+	for (y = 1; y <= sqrtLimit; ++y) {
+	    n = (4*x*x + y*y);
+	    if ((n <= limit) && (((n % 12) === 1) || ((n % 12) === 5))) {
+		isPrime[n] = !isPrime[n];
+	    }
+	    n = (3*x*x + y*y);
+	    if ((n <= limit) && ((n % 12) === 7)) {
+		isPrime[n] = !isPrime[n];
+	    }
+	    n = (3*x*x - y*y);
+	    if ((x > y) && (n <= limit) && ((n % 12) === 11)) {
+		isPrime[n] = !isPrime[n];
+	    }
+	}
+    }
+
+    /*
+      Eliminate composites by 'sieving'.
+    */
+    var k;
+    for (n = 5; n <= sqrtLimit; ++n) {
+	if (isPrime[n]) {
+	    for (k = n*n; k <= limit; k *= n) {
+		isPrime[k] = false;
+	    }
+	}
+    }
+
+    /*
+      We only have a sieve filled with booleans right now; generate the actual
+      list of prime numbers.
+    */
+    var primes = [];
+    for (var i = 2; i < isPrime.length; ++i) {
+	if (isPrime[i]) {
+	    primes.push(i);
+	}
+    }
+
+    return primes;
+};
+
+
+exports.isPrime = function (candidate)
+{
+    if ((candidate % 2) === 0) {
+	if (candidate === 2) {
+	    return true;
+	} else {
+	    return false;
+	}
+    }
+
+    var squareRoot = Math.sqrt(candidate);
+    var i = 3;
+    while (i <= squareRoot) {
+	if ((candidate % i) === 0) {
+	    return false;
+	}
+	i += 2;
+    }
+
+    return true;
+};
+
+
+exports.findNthPrime = function (n, primeFinder)
+{
+    if (n <= 1) {
+	return 2;
+    }
+
+    /*
+      If there is primeFinder function passed in, set it to a default.
+    */
+    if (typeof primeFinder === 'undefined') {
+	primeFinder = this.isPrime;
+    }
+
+    var candidate = 3;
+    var i = 1;
+    while (i < n) {
+	if (primeFinder(candidate)) {
+	    ++i;
+	}
+	candidate += 2;
+    }
+
+    return (candidate - 2);
+};
