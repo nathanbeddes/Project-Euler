a4b1540c8de7b1719538ae2d37b1b3e9d0b25275
Nathan Beddes <beddes@gmail.com>
2012-04-10 20:05:12 -0400

    Implemented problem 10 in JavaScript, based on the python version.

--- JavaScript/10.js
+++ JavaScript/10.js
@@ -8,9 +8,21 @@
  142913828922
 */
 
+var primes = require("./primes.js");
 
-var sumPrimesBelowUpper = function (upper)
+var sumPrimes30ToUpper = function (upper)
 {
+   /*
+    * We'll manually sum the primes under 30 from a table, then use the table to
+    * find the primes larger than 30.
+    */
+   var primesUnder30 = [2,3,5,7,11,13,17,19,23,29];
+
+   var sum = 0;
+   for (var i = 0; i < primesUnder30.length; ++i) {
+      sum += primesUnder30[i];
+   }
+
    /*
     * Prime numbers over 30 are of the form 30*k + i, where i are integers that
     * are coprime with 30. However, note that not all integers of the form 30*k
@@ -19,19 +31,26 @@ var sumPrimesBelowUpper = function (upper)
     * 30 was chosen because it is the 3rd primorial, or the product of the first
     * n primes, in this case 2*3*5. In general, primes are of the form
     * primorial_n*k + i, where i are coprime with primorial_n.
-    *
-    * The sum of the prime numbers under 30:
-    * 2 + 3 + 5 + 7 + 11 + 13 + 17 + 19 + 23 + 29 = 129
     */
-   var primesUnder30 = [2,3,5,7,11,13,17,19,23,29];
-   var sum = 0;
+   var coPrimesFor30 = [1,7,11,13,17,19,23,29];
+   var coPrimesFor30Count = coPrimesFor30.length;
 
-   for (var i = 0; i < primesUnder30.length; ++i) {
-      sum += primesUnder30[i];
+   var candidate = 31; 
+   for (var k = 1; candidate < upper; ++k) {
+      for (var i = 0; i < coPrimesFor30Count; ++i) {
+	 candidate = (30*k + coPrimesFor30[i]);
+	 if (candidate > upper) {
+	    return sum;
+	 }
+	 if (primes.isPrimeSophisticated(candidate, primesUnder30)) {
+//	 if (primes.isPrimeSophisticated(candidate)) {
+	    sum += candidate;
+	 }
+      }
    }
 
    return sum;
 };
 
 
-console.log(sumPrimesBelowUpper(2000000));
+console.log(sumPrimes30ToUpper(2000000));
